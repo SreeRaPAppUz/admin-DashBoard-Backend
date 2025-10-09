7ffa66b61e6e521b86c3e25b7b7f3b3a5b134d5b
Sreerag k b <sreeragkb7@gmail.com>
2025-10-09 10:23:32 +0530

    6 commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors({
   origin: [
     'https://admin-dashboardfrontend.netlify.app',
-    'http://localhost:3000'
+    'http://localhost:5173'
   ],
   credentials: true
 }));
